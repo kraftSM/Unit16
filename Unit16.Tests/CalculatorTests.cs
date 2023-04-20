6478dec4673311cc0a1d9747078b3ad384d01d3b
sergey <KraftSM@yandex.ru>
2023-04-20 16:42:18 +0300

    + Unit16.tests

--- Unit16.Tests/CalculatorTests.cs
+++ Unit16.Tests/CalculatorTests.cs
@@ -18,20 +18,13 @@ namespace Unit16.Tests
             var calculatorTest = new Calculator();
             Assert.IsNotNull(calculatorTest);
         }
-        
-        public void AddAlwaysReturnsExpectedValue()
-        {
-            var calculatorTest = new Calculator();
-            Assert.That(calculatorTest.Add(10, 220), Is.EqualTo(230));
-
-        }
-        
 
         [Test]
         public void Subtraction_MustReturnCorrectValue()
         {
             Calculator calculator = new Calculator();
             Assert.That(calculator.Subtraction(300, 10) == 290);
+            Assert.That(calculator.Subtraction(300, -10) == 310);
             Assert.That(calculator.Subtraction(300, 350) == -50);
             Assert.That(calculator.Subtraction(300, 300) == 0);
         }
@@ -40,8 +33,26 @@ namespace Unit16.Tests
         {
             Calculator calculator = new Calculator();
             Assert.That(calculator.Division(300, 10) == 30);
+            Assert.That(calculator.Division(300, -10) == -30);
             Assert.That(calculator.Division(300, 300) == 1);
             Assert.Throws<DivideByZeroException>(() => calculator.Division(30, 0));
+        }
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(30, 10) == 300);
+            Assert.That(calculator.Miltiplication(33, -10) == -330);
+            Assert.That(calculator.Miltiplication(30, 0) == 0);
+            Assert.That(calculator.Miltiplication(50, 1) == 50);
+        }
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(30, 10) == 40);
+            Assert.That(calculator.Additional(30, 0) == 30);
+            Assert.That(calculator.Additional(50, -10) == 40);
         }
     }
 }
